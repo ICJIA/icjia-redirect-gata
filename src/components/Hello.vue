5ced7e5ce0a826509bbf200e7faec02e3a564f9e
cschweda <cschweda@gmail.com>
2017-05-15 14:23:36 -0500

    initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>The ICJIA GATA website has moved.</h1>
+    <h3>Please go to <a href="https://grants.icjia.cloud">https://grants.icjia.cloud</a></h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
