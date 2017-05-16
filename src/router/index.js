8ba5040f7b2909629a86d9852e92d06705ef49f1
cschweda <cschweda@gmail.com>
2017-05-16 08:12:16 -0500

    Added default redirect route

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,22 @@ import Hello from '@/components/Hello'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+        return { x: 0, y: 0 }
+  },
+  linkActiveClass: 'active',
+  base: __dirname,
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/*',
+      name: 'Redirect',
+      redirect: '/'
     }
   ]
 })
